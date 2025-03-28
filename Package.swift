4775e0997af820847794a4d43ecd8e05262c664d
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2025-03-28 15:47:25 -0400

    chore: update verifier to v0.0.25

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TinfoilVerifier",
-            url: "https://github.com/tinfoilsh/verifier/releases/download/v0.0.22/TinfoilVerifier.xcframework.zip",
-            checksum: "6436a708ecb9b332869d2fa5e8ec2a4a48d3397411bcffeba1dc29c12dcc6c7a"),
+            url: "https://github.com/tinfoilsh/verifier/releases/download/v0.0.25/TinfoilVerifier.xcframework.zip",
+            checksum: "4848628ef47a0aa9951cec2381051e3891441f062c099180d9d2f97970a59721"),
         .target(
             name: "TinfoilKit",
             dependencies: [
