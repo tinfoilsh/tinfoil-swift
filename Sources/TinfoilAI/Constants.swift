80be8bf04ee0a26b92a957e5ce280becc5d7fc6a
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2026-01-29 20:21:44 -0500

    chore: remove unused RouterManager

--- Sources/TinfoilAI/Constants.swift
+++ Sources/TinfoilAI/Constants.swift
@@ -8,9 +8,6 @@ public enum TinfoilConstants {
     /// ATC (Attestation and Trust Center) base URL
     public static let atcBaseURL = "https://atc.tinfoil.sh"
 
-    /// ATC API URL for fetching available routers
-    public static let atcRoutersURL = "\(atcBaseURL)/routers"
-
     /// ATC API URL for fetching attestation bundles
     public static let atcAttestationURL = "\(atcBaseURL)/attestation"
 
