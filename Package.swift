79cb8f532b5fe2a24ccda2c4ddbe0c1749c5f59c
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2026-02-02 09:47:11 -0500

    chore: update openai-swift pkg

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["TinfoilAI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tinfoilsh/openai-swift-fork.git", from: "0.0.1"),
+        .package(url: "https://github.com/tinfoilsh/openai-swift-fork.git", exact: "0.0.3"),
         .package(url: "https://github.com/tinfoilsh/encrypted-http-body-protocol.git", from: "0.1.5"),
     ],
     targets: [
