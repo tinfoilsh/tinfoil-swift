fadbd0308bd4fabe62be6b3c8a9de8bf30110d69
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2026-04-26 20:05:45 -0400

    chore: update deps

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
             targets: ["TinfoilAI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tinfoilsh/openai-swift-fork.git", exact: "0.0.5"),
-        .package(url: "https://github.com/tinfoilsh/encrypted-http-body-protocol.git", from: "0.1.5"),
+        .package(url: "https://github.com/tinfoilsh/openai-swift-fork.git", exact: "0.0.6"),
+        .package(url: "https://github.com/tinfoilsh/encrypted-http-body-protocol.git", from: "0.2.0"),
     ],
     targets: [
         .binaryTarget(
